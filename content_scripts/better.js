bc6ddc4f2c4b71075a53b3681732e1bba82cdd36
Artem Soldatkin <a.soldatkin@corp.badoo.com>
2018-01-13 13:35:28 +0000

    version 0.0.1

---
+++ content_scripts/better.js
@@ -0,0 +1,49 @@
+function smart_link(event) {
+    let element = event.target;
+    if (element.href.includes('.png') && element.dataset.img !== 'true') {
+        event.preventDefault();
+        let img = document.createElement('img');
+        img.src = element.href;
+        img.title = 'Click to close';
+        img.addEventListener('click', (event) => {
+            this.dataset.img = '';
+            let item = event.target;
+            item.parentNode.removeChild(item);
+        });
+        element.parentNode.insertBefore(img, element.nextSibling);
+        element.dataset.img = 'true';
+    }
+}
+
+function step() {
+    document.querySelectorAll('.fullStacktrace').forEach((item) => {
+        if (item.innerHTML.length > 0 && (undefined === item.dataset.converted || !item.dataset.converted)) {
+            // replace links
+            item.innerHTML = item.innerHTML.replace(/(https?:\/\/[^ \s]+)([ \s])/gi, '<a href="$1" target="_blank" data-img="">$1</a>$2');
+            // replace >>anything<<
+            item.innerHTML = item.innerHTML.replace(/&gt;&gt;(.+)&lt;&lt;/gi, '>><code>$1</code><<');
+            item.dataset.converted = true;
+        }
+    });
+    document.querySelectorAll('.fullStacktrace a').forEach((item) => {
+        item.addEventListener('click', smart_link, false);
+    });
+}
+
+// select the target node
+let target = document.querySelector('#tst_group_build_fail');
+
+// create an observer instance
+let observer = new MutationObserver((mutations) => {
+    mutations.forEach(mutation => step);
+});
+
+// configuration of the observer:
+let config = {attributes: true, childList: true, subtree: true, characterData: true};
+
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
+
+window.onunload = () => {
+    observer.disconnect();
+};
