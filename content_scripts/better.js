03ff925635d80d710fc4650f0782f4e570bd0d63
leipreachan <bbidox@gmail.com>
2018-01-13 19:01:32 +0000

    version 0.0.2 - properly handle disappearing nodes

--- content_scripts/better.js
+++ content_scripts/better.js
@@ -2,6 +2,7 @@ function smart_link(event) {
     let element = event.target;
     if (element.href.includes('.png') && element.dataset.img !== 'true') {
         event.preventDefault();
+        let container = document.createElement('div');
         let img = document.createElement('img');
         img.src = element.href;
         img.title = 'Click to close';
@@ -10,19 +11,23 @@ function smart_link(event) {
             let item = event.target;
             item.parentNode.removeChild(item);
         });
-        element.parentNode.insertBefore(img, element.nextSibling);
+        element.parentNode.insertBefore(container, element.nextSibling);
+        container.appendChild(img);
         element.dataset.img = 'true';
     }
 }
 
 function step() {
     document.querySelectorAll('.fullStacktrace').forEach((item) => {
-        if (item.innerHTML.length > 0 && (undefined === item.dataset.converted || !item.dataset.converted)) {
+        if (item.innerHTML.length > 0 && item.querySelector('.test') === null) {
             // replace links
             item.innerHTML = item.innerHTML.replace(/(https?:\/\/[^ \s]+)([ \s])/gi, '<a href="$1" target="_blank" data-img="">$1</a>$2');
             // replace >>anything<<
             item.innerHTML = item.innerHTML.replace(/&gt;&gt;(.+)&lt;&lt;/gi, '>><code>$1</code><<');
-            item.dataset.converted = true;
+            // add an invisible span
+            let test = document.createElement('span');
+            test.setAttribute('class', 'test');
+            item.appendChild(test);
         }
     });
     document.querySelectorAll('.fullStacktrace a').forEach((item) => {
