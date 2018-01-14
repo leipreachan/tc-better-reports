e187e6378b6b548e2d0cdd90214bf0912ded9694
leipreachan <bbidox@gmail.com>
2018-01-14 17:23:21 +0000

    0.0.3 - handle mp4 as video

--- content_scripts/better.js
+++ content_scripts/better.js
@@ -1,19 +1,58 @@
-function smart_link(event) {
-    let element = event.target;
-    if (element.href.includes('.png') && element.dataset.img !== 'true') {
+function preview_media(event) {
+    const element = event.target;
+    const type = element.href.substr(element.href.lastIndexOf('.') + 1).toLowerCase();
+
+    function create_preview_container(opener) {
+        let preview_container = document.createElement('div');
+        preview_container.setAttribute('class', 'preview');
+        let id = (new Date()).toJSON();
+        preview_container.setAttribute('id', id);
+        opener.dataset.preview = id;
+        return preview_container;
+    }
+
+    function close_preview_container(opener) {
+        let preview_container = document.getElementById(element.dataset.preview);
+        preview_container.parentNode.removeChild(preview_container);
+        opener.dataset.preview = '';
+        opener.title = '';
+    }
+
+    let preview_container, media;
+    if (type === 'png' || type === 'mp4') {
         event.preventDefault();
-        let container = document.createElement('div');
-        let img = document.createElement('img');
-        img.src = element.href;
-        img.title = 'Click to close';
-        img.addEventListener('click', (event) => {
-            this.dataset.img = '';
-            let item = event.target;
-            item.parentNode.removeChild(item);
-        });
-        element.parentNode.insertBefore(container, element.nextSibling);
-        container.appendChild(img);
-        element.dataset.img = 'true';
+        if (element.dataset.preview.length > 0) {
+            close_preview_container(element);
+        } else {
+            preview_container = create_preview_container(element);
+            element.title = 'Click to hide the preview';
+        }
+    }
+
+    switch (type) {
+        case 'png':
+            media = document.createElement('img');
+            media.src = element.href;
+            media.title = 'Click to hide the preview';
+            preview_container.addEventListener('click', function () {
+                this.parentNode.removeChild(this);
+                element.dataset.preview = '';
+            });
+            break;
+        case 'mp4':
+            media = document.createElement('video');
+            let source = document.createElement('source');
+            media.setAttribute('controls', 'controls');
+            media.setAttribute('height', '500');
+            source.setAttribute('src', element.href);
+            source.setAttribute('kind', 'video/mp4');
+            media.appendChild(source);
+            break;
+    }
+
+    if (preview_container && media) {
+        preview_container.appendChild(media);
+        element.parentNode.insertBefore(preview_container, element.nextSibling);
     }
 }
 
@@ -21,7 +60,7 @@ function step() {
     document.querySelectorAll('.fullStacktrace').forEach((item) => {
         if (item.innerHTML.length > 0 && item.querySelector('.test') === null) {
             // replace links
-            item.innerHTML = item.innerHTML.replace(/(https?:\/\/[^ \s]+)([ \s])/gi, '<a href="$1" target="_blank" data-img="">$1</a>$2');
+            item.innerHTML = item.innerHTML.replace(/(https?:\/\/[^ \s]+)([ \s])/gi, '<a href="$1" target="_blank" data-preview="">$1</a>$2');
             // replace >>anything<<
             item.innerHTML = item.innerHTML.replace(/&gt;&gt;(.+)&lt;&lt;/gi, '>><code>$1</code><<');
             // add an invisible span
@@ -31,19 +70,21 @@ function step() {
         }
     });
     document.querySelectorAll('.fullStacktrace a').forEach((item) => {
-        item.addEventListener('click', smart_link, false);
+        item.addEventListener('click', preview_media, false);
     });
 }
 
-const nodes = ['#tst_group_build_fail', '#tst_group_build_muteRefresh'];
+const nodes = ['#tst_group_build_fail', '#tst_group_build_mute'];
 
-nodes.every((node)=>{
+nodes.every((node) => {
     // select the target node
     let target = document.querySelector(node);
 
 // create an observer instance
     let observer = new MutationObserver((mutations) => {
-        mutations.forEach(mutation => {step();});
+        mutations.forEach(mutation => {
+            step();
+        });
     });
 
 // configuration of the observer:
