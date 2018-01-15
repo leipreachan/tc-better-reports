773c5eb410faed865b8d4634c6dcfa3991da711e
Artem Soldatkin <a.soldatkin@corp.badoo.com>
2018-01-15 13:46:24 +0000

    0.0.4 - to monitor proper node

---
+++ build_chrome.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "not implemented yet"
+
+exit 1
+
+cp manifest.json{,.firefox} && cp manifest.chrome.json manifest.json
+
+// build here
+
+cp manifest.json.firefox manifest.json
---
+++ build_firefox.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+web-ext build --artifacts-dir="../web-ext-artifacts/"
